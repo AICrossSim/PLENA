c49885408acfb1ec2f17a27f07ccddc19b809623
Haoran Wu <georgewu1204@gmail.com>
2025-01-29 16:00:54 +0000

    0129

---
+++ doc/ISAs_for_Llama.asm
@@ -0,0 +1,13 @@
+; Assume Dimension
+; hidden_size = h = 4096
+; sequence_length = s_q = 1
+; floating_type = FP8 = 8 bits
+; fixed data_type = fixed8 = 8bits
+
+
+; Initializing the processor ... 
+
+; Preparing the embedded data
+
+
+; <--------------------RMS Norm------------------------>
