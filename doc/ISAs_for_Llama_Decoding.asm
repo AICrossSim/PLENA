ad4b1e451036bc05ac4fa70b2a92a63996ca3895
GeorgeWu1204 <georgewu1204@gmail.com>
2025-01-31 16:14:18 +0000

    ss

--- doc/ISAs_for_Llama_Decoding.asm
+++ doc/ISAs_for_Llama_Decoding.asm
@@ -40,8 +40,8 @@ LOOP_N_Layers:
     // <--------------------RMS Norm------------------------>
     addi x1, x0, (hidden_size / MLEN);
     mv x2, x0;
-    v.fmul.vf v1, x0;                       Zero mapping v1
-    v.fmul.vf v3, x0;                       Zero mapping v3
+    v.fmul.vf v1, f0;                       Zero mapping v1
+    v.fmul.vf v3, f0;                       Zero mapping v3
 
     LOOP_MLEN_VEC1:
         v.lv v1, x2, 0;                     Loading vector data of MLEN stored in SRAM to vector reg v1;
@@ -172,6 +172,7 @@ LOOP_N_Layers:
     // <-------------------- FlashAttention ------------------------>
     lw x10, ; (s_num)
     mv x11, ;  address for q_new (1, num_attention_heads, head_dim) in SSRAM
+    addi x13, x11, head_dim * num_attention_heads;  address for s (1, Bc) in SSRAM
 
     LOOP_ATTENTION_Q_HEADS:
         lui x2, ; (index of in q head // num_head_groups)
@@ -183,6 +184,7 @@ LOOP_N_Layers:
             mv x7, x0;                      x7 stores the address in MVM SRAM
             mv x8, x0;                      x8 stores the address for sequence_index in HBM
             mv x9, x0;                      x9 stores the address offset in HBM
+            lui x12, 0x0;                   ith tile in the matrix
             // Fetching K of ((Bc), specified_idx, head_dim) from HBM () to MVM SRAM
             LOOP_Bc:
                 mv          x9, x8;
@@ -190,9 +192,40 @@ LOOP_N_Layers:
                 m.fetch     x7, x9, csr_adr[3];
                 addi        x8, x8, head_dim * num_key_value_heads;
                 addi        x7, x7, head_dim;
+                addi        x12, x12, 0x1;
+                blt         x12, x6, LOOP_Bc;
 
             // Fetching Q from SSRAM (1, head_dim) and Q @ K^T (Bc, head_dim)
+            mv x7, x0;                      x7 stores the address in MVM SRAM
+            lui x12, 0x0;                   ith tile in the matrix
+            lui x13, Num_of_Tiles_Head; 
             LOOP_MLEN_MATRIX:
+                m.mv x7, x12, x11;
+                addi x7, x7, MLEN;
+                addi x12, x12, 0x1;
+                blt x12, x13, LOOP_MLEN_MATRIX;
+            m.extract x13;
+
+            // s_j = q @ k_j.transpose(1, 2) * qk_scale
+            lui x12, 0x0;                   ith tile in the matrix
+            lui x13, Num_of_Tiles_Head; 
+            lui f2, qk_scale;
+            LOOP_MLEN_VECTOR:
+                v.lv v1, x13, 0;
+                v.fmul.vf v2, v1, f2;
+                v.fredmax v3, v3, v2;
+                v.sv v2, x13, 0;
+                addi x13, x13, MLEN;
+
+            // rowmax_s_j = s_j.max().cpu().item()
+            v.fmv.f.s f2, v3;             Storing the max value of s_j in x2 Note, storing results to floating type registers.
+            lui x3, (memory to store previous m);
+            ld f1, x3, 0;
+            fmax.s f1, f1, f2;
+
+            
+
+
             
                 
 
