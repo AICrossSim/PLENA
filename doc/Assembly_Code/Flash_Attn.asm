152dec8f4316dcca66894e46e2beb9f2573e634a
Haoran Wu <georgewu1204@gmail.com>
2025-02-04 04:18:14 +0000

    estimation

--- doc/Assembly_Code/Flash_Attn.asm
+++ doc/Assembly_Code/Flash_Attn.asm
@@ -1,49 +1,59 @@
 // <-------------------- FlashAttention ------------------------>
-    LOOP_ATTENTION_Q_HEADS:
-        lui x2, ; (index of in q head // num_head_groups)
-        lui x3, head_dim;
-        mul x4, x2, x3;                memory offset to csr_adr[3].
-        lui x5, Tc;
-        lui x6, Bc;
-        LOOP_ATTENTION_PER_HEAD:
-            mv x7, x0;                      x7 stores the address in MVM SRAM
-            mv x8, x0;                      x8 stores the address for sequence_index in HBM
-            mv x9, x0;                      x9 stores the address offset in HBM
-            lui x12, 0x0;                   ith tile in the matrix
-            // Fetching K of ((Bc), specified_idx, head_dim) from HBM () to MVM SRAM
-            LOOP_Bc:
-                mv          x9, x8;
-                addi        x9, x9, x4;
-                m.fetch     x7, x9, csr_adr[3];
-                addi        x8, x8, head_dim * num_key_value_heads;
-                addi        x7, x7, head_dim;
-                addi        x12, x12, 0x1;
-                blt         x12, x6, LOOP_Bc;
+// Assuming the q is in shape [1, 1, num_attention_heads, Head_Dim]
+// Assuming the k is in shape [1, num_key_value_heads, s_kv, Head_Dim]
+// Assuming the v is in shape [1, num_key_value_heads, s_kv, Head_Dim]
+lui x1, Head_Dim;
+lui x2, s_kv//Tile_Size;        Tc Filled by Compiler
+lui x3, Tile_Size;              Bc
+lui x4, s_kv;                   Sequence Length
+mv x5, x0;                      index of q_head.
+LOOP_ATTENTION_Q_HEADS:
+    div x5, ; (index of in q head // num_head_groups) Filled by Compiler, index of kv_head.
+    mul x7, x4, x1;                
+    mul x7, x7, x5;                     memory offset to the ith head content in K, V in HBM
+    mv x8, x0;                          memory offset to the ith sequence content in K, V in HBM
+    lui x9, Tile_Size * Tile_Size;
 
-            // Fetching Q from SSRAM (1, head_dim) and Q @ K^T (Bc, head_dim)
-            mv x7, x0;                      x7 stores the address in MVM SRAM
-            lui x12, 0x0;                   ith tile in the matrix
-            lui x13, Num_of_Tiles_Head; 
-            LOOP_MLEN_MATRIX:
-                m.mv x7, x12, x11;
-                addi x7, x7, MLEN;
-                addi x12, x12, 0x1;
-                blt x12, x13, LOOP_MLEN_MATRIX;
-            m.extract x13;
+    LOOP_ATTENTION_PER_HEAD:
+        // Q @ K^T  
+        // Fetching K and Q of (1, specified_idx, (Bc), len(Head_Dim)) from HBM () to MVM SRAM
+        lui x12, Head_Dim//Tile_Size;
 
-            // s_j = q @ k_j.transpose(1, 2) * qk_scale
-            lui x12, 0x0;                   ith tile in the matrix
-            lui x13, Num_of_Tiles_Head; 
-            lui f2, qk_scale;
-            LOOP_MLEN_VECTOR:
-                v.lv v1, x13, 0;
-                v.fmul.vf v2, v1, f2;
-                v.fredmax v3, v3, v2;
-                v.sv v2, x13, 0;
-                addi x13, x13, MLEN;
+        LOOP_Bc:
+            add x13, x7, x8;
+            m.fetch     x13, csr_adr[3];    
+            v.fetch     x0, x0, csr_adr[8];
+            mv          x9, x8;
+            addi        x9, x9, x4;
+            addi        x8, x8, Head_Dim * num_key_value_heads;
+            addi        x7, x7, Head_Dim;
+            addi        x12, x12, 0xfff;
+            blt         x0, x12, LOOP_Bc;
 
-            // rowmax_s_j = s_j.max().cpu().item()
-            v.fmv.f.s f2, v3;             Storing the max value of s_j in x2 Note, storing results to floating type registers.
-            lui x3, (memory to store previous m);
-            ld f1, x3, 0;
-            fmax.s f1, f1, f2;
+        // Fetching Q from SSRAM (1, Head_Dim) and Q @ K^T (Bc, Head_Dim)
+        mv x7, x0;                      x7 stores the address in MVM SRAM
+        lui x12, 0x0;                   ith tile in the matrix
+        lui x13, Num_of_Tiles_Head; 
+        LOOP_MLEN_MATRIX:
+            m.mv x7, x12, x11;
+            addi x7, x7, MLEN;
+            addi x12, x12, 0x1;
+            blt x12, x13, LOOP_MLEN_MATRIX;
+        m.extract x13;
+
+        // s_j = q @ k_j.transpose(1, 2) * qk_scale
+        lui x12, 0x0;                   ith tile in the matrix
+        lui x13, Num_of_Tiles_Head; 
+        lui f2, qk_scale;
+        LOOP_MLEN_VECTOR:
+            v.lv v1, x13, 0;
+            v.fmul.vf v2, v1, f2;
+            v.fredmax v3, v3, v2;
+            v.sv v2, x13, 0;
+            addi x13, x13, MLEN;
+
+        // rowmax_s_j = s_j.max().cpu().item()
+        v.fmv.f.s f2, v3;             Storing the max value of s_j in x2 Note, storing results to floating type registers.
+        lui x3, (memory to store previous m);
+        ld f1, x3, 0;
+        fmax.s f1, f1, f2;
--- doc/performance_estimation.py
+++ doc/performance_estimation.py
@@ -11,7 +11,6 @@ class model_config:
         self.num_key_value_heads = model_param["num_key_value_heads"]
         self.attention_bias = model_param["attention_bias"]
         self.max_position_embeddings = model_param["max_position_embeddings"]
-        self.mlp_bias = model_param["mlp_bias"]
         self.head_dim = self.hidden_size // self.num_attention_heads
         self.num_head_groups = self.num_attention_heads // self.num_key_value_heads
         self.vocab_size = model_param["vocab_size"]
@@ -37,7 +36,7 @@ class model_config:
         # -- Projection
         setting_inst_num = 9
         mvm_inst_num = 13
-        loop_mvm_num = (self.hidden_size // self.Tile_Size) ^ 2
+        loop_mvm_num = (self.hidden_size // self.Tile_Size) ** 2
         data_transfer_inst_num = 5
         # Load Sin and Cos
         load_cos_sin_inst_num = 5 * (self.head_dim // self.Tile_Size)
@@ -49,7 +48,7 @@ class model_config:
         # -- Projection
         setting_inst_num = 9
         mvm_inst_num = 13
-        loop_mvm_num = (self.num_key_value_heads * self.head_dim // self.Tile_Size) ^ 2
+        loop_mvm_num = ((self.num_key_value_heads * self.head_dim) // self.Tile_Size) ** 2
         data_transfer_inst_num = 5
         # Load Sin and Cos
         load_cos_sin_inst_num = 5 * (self.head_dim // self.Tile_Size)
@@ -61,15 +60,86 @@ class model_config:
         # -- Projection
         setting_inst_num = 9
         mvm_inst_num = 13
-        loop_mvm_num = (self.num_key_value_heads * self.head_dim // self.Tile_Size) ^ 2
+        loop_mvm_num = ((self.num_key_value_heads * self.head_dim) // self.Tile_Size) ** 2
         data_transfer_inst_num = 5
         overall_inst_num += setting_inst_num + mvm_inst_num * loop_mvm_num + data_transfer_inst_num
         return overall_inst_num
 
+    def flash_attention(self):
+        overall_inst_num = 0
+        # -- Attention
+        q_heads = self.num_attention_heads
+        settings_in_each_attention = 7
+        internel_Tc_Loop = 50 // self.Tile_Size # Assuming 50 for s_kv
+
+        # Internel Tc Loop
+        # Q_KT Loop
+        Tile_Loop_per_head = self.head_dim // self.Tile_Size
+        Q_KT_inst_num = 15 * Tile_Loop_per_head  + 7 * Tile_Loop_per_head # One for QKT another for Vector elementwise qk_scale
+        # Row Max
+        row_max_inst_num = 7 * Tile_Loop_per_head
+        # Reduction by m_new
+        reduction_inst_num = 7 * Tile_Loop_per_head
+        # Exp
+        exp_inst_num = 8 * Tile_Loop_per_head
+        # Scalar
+        scalar_inst_num = 15
+        # Psum
+        psum_inst_num = 7 * Tile_Loop_per_head
+        # p v
+        p_v_inst_num = 23 * Tile_Loop_per_head
+
+        overall_inst_num += q_heads * (settings_in_each_attention + internel_Tc_Loop * (Q_KT_inst_num + row_max_inst_num + reduction_inst_num + exp_inst_num + scalar_inst_num + psum_inst_num + p_v_inst_num))
+        return overall_inst_num
 
 
+    def residual (self):
+        overall_inst_num = 0
+        # -- Residual
+        iteration = self.hidden_size // self.Tile_Size
+        overall_inst_num = 10 * iteration
+        return overall_inst_num
+
+    def feed_forward(self):
+        overall_inst_num = 0
+        # -- Feed Forward
+        setting_inst_num = 9
+        mvm_inst_num = 13
+        loop_mvm_num = (self.hidden_size * self.hidden_size) // (self.Tile_Size * self.Tile_Size)
+        data_transfer_inst_num = 5
+        overall_inst_num = setting_inst_num + (mvm_inst_num + data_transfer_inst_num) * loop_mvm_num
+        return overall_inst_num
+
+    def mlp(self):
+        overall_inst_num = 0
+        # -- MLP
+        setting_inst_num = 9
+        mvm_inst_num = 13
+        loop_mvm_num = (self.hidden_size * self.intermediate_size) // (self.Tile_Size * self.Tile_Size)
+        data_transfer_inst_num = 5
+        overall_inst_num = setting_inst_num + (mvm_inst_num + data_transfer_inst_num) * loop_mvm_num
+        return overall_inst_num
 
 
+    def compute_overall_inst(self):
+        overall_inst_num = 0
+        for i in range(self.num_hidden_layers):
+            overall_inst_num += self.rms_layer()
+            overall_inst_num += self.projection()
+            overall_inst_num += self.flash_attention()
+            overall_inst_num += self.residual()
+            overall_inst_num += self.rms_layer()
+            overall_inst_num += self.feed_forward()
+            overall_inst_num += self.residual()
+        overall_inst_num += self.rms_layer()
+        overall_inst_num += self.mlp()
+        print("Overall instruction number: ", overall_inst_num)
+        return overall_inst_num
+
+
+if __name__ == "__main__":
+    model = model_config("Model_Lib/llama-3.1-70b.json")
+    print(model.compute_overall_inst())
 
 
 
