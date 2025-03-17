d4824957fcc7a83f8bf960835ca3a7753bce87f6
GeorgeWu1204 <georgewu1204@gmail.com>
2025-03-17 21:12:56 +0000

    0317

---
+++ src/matrix_machine/rtl/matrix_machine.sv
@@ -0,0 +1,20 @@
+`timescale 1ns / 1ps
+
+module matrix_machine #(
+    parameter MLEN = 4,
+    parameter MAN_WIDTH = 8,
+    parameter EXP_WIDTH = 4,
+) (
+    input logic clk,
+    input logic rst,
+    
+    // TileLink SLAVE
+
+    // TileLink MASTER
+);
+
+
+
+
+
+endmodule
