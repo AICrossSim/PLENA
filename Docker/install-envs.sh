6907d48da3131ae3a68c96b7c7331cbc799e52b2
cx922 <cx922@ic.ac.uk>
2025-04-22 10:18:01 +0100

    Docker Env

---
+++ Docker/install-envs.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+# --------------------------------------------------------------------
+#    This script installs environment vars for both Docker containers 
+# --------------------------------------------------------------------
+set -o errexit
+set -o pipefail
+set -o nounset
