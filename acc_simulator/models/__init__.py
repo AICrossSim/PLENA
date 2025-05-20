85e46caa8b3cbb16a312f602035da81ce687d8bf
jiaeenie <selinaniebakers@gmail.com>
2025-05-20 22:24:56 +0100

    Add initial quantized llama model implementation based on Transformers v4.51.3

---
+++ acc_simulator/models/__init__.py
@@ -0,0 +1,31 @@
+from transformers import LlamaTokenizer  
+
+from .llama_quantized import LlamaQuantizedConfig, LlamaQuantizedForCausalLM
+
+MODEL_MAP = {
+    "llama": {
+        "lm": LlamaQuantizedForCausalLM
+    }
+}
+
+CONFIG_MAP = {
+    "llama": LlamaQuantizedConfig
+}
+
+TOKENIZER_MAP = {
+    "llama": LlamaTokenizer
+}
+
+
+def get_model_cls(arch: str, task: str):
+    assert arch in MODEL_MAP, f"arch {arch} not supported"
+    assert task in MODEL_MAP[arch], f"task {task} not supported for arch {arch}"
+    return MODEL_MAP[arch][task]
+
+def get_config_cls(arch: str):
+    assert arch in CONFIG_MAP, f"arch {arch} not supported"
+    return CONFIG_MAP[arch]
+
+def get_tokenizer_cls(arch: str):
+    assert arch in TOKENIZER_MAP, f"arch {arch} not supported"
+    return TOKENIZER_MAP[arch]
---
+++ acc_simulator/models/llama_quantized/__init__.py
@@ -0,0 +1,2 @@
+from .modeling_llama_quantized import *
+from .configuration_llama_quantized import *
