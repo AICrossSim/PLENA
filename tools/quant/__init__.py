e44b07c14fe62e8f3ed83eb6353187f2f8ac18db
jiaeenie <selinaniebakers@gmail.com>
2025-05-20 21:45:55 +0100

    Add  file for quant tools package

---
+++ tools/quant/__init__.py
@@ -0,0 +1,12 @@
+from .quantized_functions import QUANTIZED_FUNC_MAP
+from .quantized_modules import QUANTIZED_MODULE_MAP
+from .quantizer import QUANTIZER_MAP
+
+def get_quantized_cls(op: str, config: dict):
+    return QUANTIZED_MODULE_MAP[op][config["name"]]
+
+def get_quantized_func(op: str, config: dict):
+    return QUANTIZED_FUNC_MAP[op][config["name"]]
+
+def get_quantizer(op: str, config: dict):
+    return QUANTIZER_MAP[config["name"]]
--- tools/quant/quantizer/__init__.py
+++ tools/quant/quantizer/__init__.py
@@ -1,6 +1,11 @@
-
 from .mxint import mx_int_quantizer
 from .minifloat import minifloat_ieee_quantizer
 from .mxfp import mxfp_quantizer
 
 __all__ = ["mx_int_quantizer", "minifloat_ieee_quantizer", "mxfp_quantizer"]
+
+QUANTIZER_MAP = {
+    "mx_int": mx_int_quantizer,
+    "minifloat_ieee": minifloat_ieee_quantizer,
+    "mxfp": mxfp_quantizer
+}
