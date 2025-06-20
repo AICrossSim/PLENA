5fe5a4ba7254fa78603cd8714d09b54eb818f845
Cano Xiao <cx922@ic.ac.uk>
2025-06-20 15:09:46 +0100

    finish hash_non_linear

--- src/basic_components/fp_operation/rtl/fp_hash_reciprocal.sv
+++ src/basic_components/fp_operation/rtl/fp_hash_reciprocal.sv
@@ -1,12 +1,12 @@
 
 `timescale 1ns / 1ps
 /*
-Module      : fp_hash_exp
+Module      : fp_hash_reciprocal
 Timing      : Combinatorial Logic
-Description : FP_Hash_Exp
-            - Performs hash operation on the exponent of the input floating-point number.
+Description : FP_Hash_Reciprocal
+            - Performs hash operation on the reciprocal of the input floating-point number.
 */
-module fp_hash_exp #(
+module fp_hash_reciprocal #(
     parameter int IN_EXP_WIDTH = 5,
     parameter int IN_MANT_WIDTH = 10,
     // Amount of bits needed to shift mantissas for alignment
@@ -17,10 +17,10 @@ module fp_hash_exp #(
     output logic signed [OUT_EXP_WIDTH + OUT_MANT_WIDTH:0] data_out
 );
 
-    exp_lut #(
+    reciprocal_lut #(
         .IN_ENTRY_WIDTH(IN_EXP_WIDTH + IN_MANT_WIDTH + 1),
         .OUT_ENTRY_WIDTH(OUT_EXP_WIDTH + OUT_MANT_WIDTH + 1)
-    ) exp_lut_inst (
+    ) reciprocal_lut_inst (
         .data_in(data_in),
         .data_out(data_out)
     );
--- src/basic_components/fp_operation/rtl/fp_hash_sqrt.sv
+++ src/basic_components/fp_operation/rtl/fp_hash_sqrt.sv
@@ -1,12 +1,12 @@
 
 `timescale 1ns / 1ps
 /*
-Module      : fp_hash_exp
+Module      : fp_hash_sqrt
 Timing      : Combinatorial Logic
-Description : FP_Hash_Exp
-            - Performs hash operation on the exponent of the input floating-point number.
+Description : FP_Hash_Sqrt
+            - Performs hash operation on the square root of the input floating-point number.
 */
-module fp_hash_exp #(
+module fp_hash_sqrt #(
     parameter int IN_EXP_WIDTH = 5,
     parameter int IN_MANT_WIDTH = 10,
     // Amount of bits needed to shift mantissas for alignment
@@ -17,10 +17,10 @@ module fp_hash_exp #(
     output logic signed [OUT_EXP_WIDTH + OUT_MANT_WIDTH:0] data_out
 );
 
-    exp_lut #(
+    sqrt_lut #(
         .IN_ENTRY_WIDTH(IN_EXP_WIDTH + IN_MANT_WIDTH + 1),
         .OUT_ENTRY_WIDTH(OUT_EXP_WIDTH + OUT_MANT_WIDTH + 1)
-    ) exp_lut_inst (
+    ) sqrt_lut_inst (
         .data_in(data_in),
         .data_out(data_out)
     );
