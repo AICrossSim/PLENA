66b5459c30162a4d645d84e89e338837bb48b4d2
Cano Xiao <cx922@ic.ac.uk>
2025-07-13 11:25:31 +0100

    add assembler

--- test/Instr_Level_Benchmark/vector_fp_add.asm
+++ test/Instr_Level_Benchmark/vector_fp_add.asm
@@ -1,13 +1,17 @@
 
-// testing for HBM[2] = HBM[1] + HBM[0]
+S_LUI_FIX fx_x1, 0; 
+// x1 = 0 << 12
+C_SET_ADDR_REG addr_x1, fx_x1, fx_x1; 
+H_PREFETCH_V_C fx_x1, fx_x1, addr_x1; 
+// HBM[addr_x2 + 0] -> VSRAM[x1]
 
-S_LUI_FIX x1, 0; // x1 = 0 << 12
-S_LUI_FIX x2, 1; // x2 = 1 << 12
-C_SET_ADDR_REG x1, x1, x2; // {x1, x1} -> addr_x2
-H_PREFETCH_V_C x2, x1, x2; // HBM[x1+addr_x2] -> VSRAM[x2]
-V_ADD_VV x1, x1, x2; // VSRAM[x1] + VSRAM[x2] -> VSRAM[x3]
-S_LUI_FIX x3, 1; // x3 = 1 << 12
+S_LD_FP fp_x1, fx_x1, 0; 
+V_ADD_VF fx_x1, fx_x1, fp_x1; 
+// VSRAM[x3(1)] = VSRAM[x1(0)](x) + 1
+V_ADD_VV fx_x1, fx_x1, fx_x1; 
+// VSRAM[x3(1)] = VSRAM[x1(0)](x) + VSRAM[x3(1)](x + 1)
 
-H_STORE_V_H_C x1, x2, x2; // VSRAM [FIX_REG[x3]]= VSRAM[x1]
-
-H_PREFETCH_V_C
+S_LUI_FIX fx_x2, 1; 
+// x2 = 1
+H_STORE_V_H_C fx_x1, fx_x2, addr_x2; 
+// HBM[addr_x2 + 1] = VSRAM[x3]
