50de94d0a1eeaa898e2c1bd04fd5b988c8f333e9
GeorgeWu1204 <georgewu1204@gmail.com>
2025-07-17 23:53:10 +0100

    fix bugs for configurable scalar machine

--- src/definitions/configuration.svh
+++ src/definitions/configuration.svh
@@ -7,12 +7,12 @@ import precision_pkg::*;
 
 package configuration_pkg;
     // Compute Unit Related 
-    parameter   BLEN = 4;
-    parameter   MLEN = 8;
+    parameter   BLEN = 32;
+    parameter   MLEN = 128;
     parameter   Matrix_Parallel_Rd_Dim = 1;
     parameter   VLEN = MLEN;
     parameter   INST_BUFF_DEPTH = 16;
-    parameter   ON_CHIP_ADDR_WIDTH = 32;
+    parameter   ON_CHIP_ADDR_WIDTH = precision_pkg::INT_DATA_WIDTH;
     parameter   HBM_ADDR_WIDTH = 128;
     parameter   SourceWidth = 1;
     parameter   SinkWidth = 1;
@@ -31,7 +31,7 @@ package configuration_pkg;
     parameter   HBM_M_Prefetch_Amount = 16;
     parameter   HBM_V_Prefetch_Amount = 16;
     parameter   HBM_V_Writeback_Amount = 16;
-    parameter   HBM_ELE_WIDTH   = 512;
+    parameter   HBM_ELE_WIDTH = 512;
     parameter   HBM_SCALE_WIDTH = 512;
 endpackage
 
