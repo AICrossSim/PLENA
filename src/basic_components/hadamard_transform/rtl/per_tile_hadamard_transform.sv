c65026181b7957473c777f68fd9d65c0bef8a40e
GeorgeWu1204 <georgewu1204@gmail.com>
2025-07-30 20:00:59 +0100

    testbench for per tile hadmard transform

--- src/basic_components/hadamard_transform/rtl/per_tile_hadamard_transform.sv
+++ src/basic_components/hadamard_transform/rtl/per_tile_hadamard_transform.sv
@@ -1,7 +1,8 @@
 `timescale 1ns / 1ps
+`include "global_define.vh"
 
 /*
-Module      : Per Tile Hadamard Transform RTL 
+Module      : FP Per Tile Hadamard Transform RTL 
 Timing      : Sequential Logic
 Status      : Full Pipelined Version Hadamard Transform RTL
 */
@@ -22,33 +23,35 @@ module per_tile_hadamard_transform #(
 
     localparam int TRANSFORM_STAGES = $clog2(TILESIZE);
     logic [TRANSFORM_STAGES : 0][TILESIZE - 1: 0][EXP_WIDTH + MANT_WIDTH : 0] data_reg;
-    logic [TRANSFORM_STAGES : 0][TILESIZE - 1: 0] data_valid_reg;
-    logic [TRANSFORM_STAGES : 0][TILESIZE - 1: 0] data_ready_reg;
+    logic [TILESIZE - 1: 0][EXP_WIDTH + MANT_WIDTH : 0] stage_0_reg;
+    logic [TRANSFORM_STAGES : 0] data_valid_reg;
 
-    always_ff begin
+    always_ff @(posedge clk) begin
         if (rst) begin
-            data_reg <= '0;
-            data_valid_reg <= '0;
-            data_ready_reg <= '0;
+            data_valid_reg <= 1'b0;
+            stage_0_reg    <= '0;
         end else begin
             if (data_in_valid) begin
-                data_reg[0]         <= data_in;
+                stage_0_reg         <= data_in;
                 data_valid_reg[0]   <= 1'b1;
             end else begin
+                stage_0_reg         <= '0;
                 data_valid_reg[0]   <= 1'b0;
-                data_reg[0]         <= '0;
+            end
+            for (int i = 0; i < TRANSFORM_STAGES; i++) begin
+                data_valid_reg[i + 1] <= data_valid_reg[i];
             end
         end
     end
     
     // Pipeline registers for each stage
+    assign data_reg[0] = stage_0_reg;
     genvar h, i, j;
-
     generate
         for (h = 1; h < TILESIZE + 1; h = h * 2) begin : loop_h
             localparam int STAGE = $clog2(h+1);
             for (i = 0; i < TILESIZE; i = i + h) begin : loop_i
-                for (j = i, j < i + h; j ++) begin
+                for (j = i; j < i + h; j ++) begin
                     logic [EXP_WIDTH + MANT_WIDTH : 0] negated_data_b;
                     assign negated_data_b = {data_reg[STAGE][j+h][EXP_WIDTH + MANT_WIDTH], 
                                             ~data_reg[STAGE][j+h][EXP_WIDTH + MANT_WIDTH - 1:0]};
@@ -58,7 +61,7 @@ module per_tile_hadamard_transform #(
                     ) fp_add (
                         .clk(clk),
                         .rst(rst),
-                        .data_in_valid  (data_in_valid_reg[STAGE]),
+                        .data_in_valid  (data_valid_reg[STAGE]),
                         .data_a         (data_reg[STAGE][j]),
                         .data_b         (data_reg[STAGE][j + h]),
                         .data_out       (data_reg[STAGE + 1][j]),
@@ -71,20 +74,13 @@ module per_tile_hadamard_transform #(
                     ) fp_sub (
                         .clk(clk),
                         .rst(rst),
-                        .data_in_valid  (data_in_valid_reg[STAGE]),
+                        .data_in_valid  (data_valid_reg[STAGE]),
                         .data_a         (data_reg[STAGE][j]),
-                        .data_b         (negated_data_b[STAGE][j + h]),
+                        .data_b         (negated_data_b),
                         .data_out       (data_reg[STAGE + 1][j + h]),
                         .data_out_valid ()
                     );
                 end              
-            end
-            always_ff @(posedge clk or posedge rst) begin
-                if (rst) begin
-                    data_in_valid_reg[STAGE] <= 1'b0;
-                end else begin
-                    data_in_valid_reg[STAGE] <= data_in_valid;
-                end
             end
         end
     endgenerate
