db9174f9e7094267e8fb8183351d7ba27cf3142b
jiaeenie <selinaniebakers@gmail.com>
2025-09-15 02:07:05 +0100

    adapt from mase-triton for minifloat

--- acc_simulator/quantize/quantizer/minifloat/__init__.py
+++ acc_simulator/quantize/quantizer/minifloat/__init__.py
@@ -1,4 +1,4 @@
-from .meta import MinifloatMeta
 from .minifloat import minifloat_quantizer_sim
+from .meta import MinifloatMeta, MinifloatTensorMeta
 
-__all__ = ["MinifloatMeta"]
+__all__ = ["MinifloatMeta", "MinifloatTensorMeta", "minifloat_quantizer_sim"]
--- acc_simulator/quantize/quantizer/minifloat/minifloat.py
+++ acc_simulator/quantize/quantizer/minifloat/minifloat.py
@@ -1,115 +1,90 @@
 import torch
 from torch import Tensor
 
-from . import fake as minifloat_fake
-from .helpers import flatten_for_quantize, permute_for_dequantize
+from ..utils import device_str, dtype_str, shape_tuple
+from . import fake as fp_fake
+from . import kernels as fp_kernels
 from .meta import MinifloatMeta, MinifloatTensorMeta
 
 
-def extract_minifloat_components(
+def extract_minifloat_component(
     tensor: Tensor, minifloat_meta: MinifloatMeta
-) -> MinifloatTensorMeta:
+) -> tuple[Tensor, MinifloatTensorMeta]:
     """
-    Extracts the Minifloat components from a tensor.
+    Extract the minifloat component from a tensor.
 
-    .. note::
-        The block for exponent sharing is a 1D vector instead of a 2D matrix.
-
-    :param tensor: The input tensor to be quantized.
+    :param tensor: The input tensor to extract minifloat components from.
     :type tensor: torch.Tensor
-    :param minifloat_meta: The metadata for the Minifloat format.
+    :param minifloat_meta: The metadata for the minifloat format.
     :type minifloat_meta: MinifloatMeta
-
-    :returns: The extracted scales, elements, and tensor metadata.
-    :rtype: tuple[torch.Tensor, torch.Tensor, MinifloatTensorMeta]
+    :returns: The extracted element (uint16 tensor) and tensor metadata.
+    :rtype: tuple[torch.Tensor, MinifloatTensorMeta]
     """
-    device = str(tensor.device)
-    ori_shape = tuple(tensor.shape)
-    ori_dtype = str(tensor.dtype).removeprefix("torch.")
-    ndim = len(ori_shape)
+    device = device_str(tensor.device)
+    ori_shape = shape_tuple(tensor.shape)
+    ori_dtype = dtype_str(tensor.dtype)
 
-    assert device.startswith("cpu") or device.startswith("cuda"), (
-        f"Unsupported device: {device}. Only 'cpu' and 'cuda' are supported."
-    )
-    tensor = tensor.to(torch.bfloat16)
-    if device == "cpu":
-        tensor = minifloat_fake.extract_minifloat_component(
-            tensor, minifloat_meta=minifloat_meta
-        )
+    tensor = tensor.to(torch.float32)
+
+    if device.startswith("cuda"):
+        element = fp_kernels.extract_minifloat_component(tensor, minifloat_meta)
     else:
-        tensor = minifloat_fake.extract_minifloat_component(
-            tensor, minifloat_meta=minifloat_meta
-        )
+        element = fp_fake.extract_minifloat_component(tensor, minifloat_meta)
     tensor_meta = MinifloatTensorMeta(
-        device=device,
-        dtype=ori_dtype,
-        shape=ori_shape,
-        meta=minifloat_meta,
+        device=device, dtype=ori_dtype, shape=ori_shape, meta=minifloat_meta
     )
-    return tensor, tensor_meta
+    return element, tensor_meta
 
 
-def compose_minifloat_tensor(
-    elements,
+def compose_minifloat_component(
+    element: Tensor,
     tensor_meta: MinifloatTensorMeta,
-    dtype: torch.dtype | None = None,
+    output_dtype: torch.dtype | None = None,
 ) -> Tensor:
     """
-    Compose a tensor from Minifloat components.
-
-    :param scales: The shared scales for exponent sharing.
-    :type scales: torch.Tensor
-    :param elements: The elements of the tensor.
-    :type elements: torch.Tensor
-    :param tensor_meta: The metadata for the Minifloat tensor.
+    Compose a tensor from minifloat components.
+    :param element: The element of the minifloat tensor.
+    :type element: torch.Tensor
+    :param tensor_meta: The metadata for the minifloat tensor.
     :type tensor_meta: MinifloatTensorMeta
-    :param dtype: The desired data type of the output tensor, by default None, which uses the dtype from tensor_meta.
+    :param dtype: The desired data type of the output tensor, by default None, which
+        uses the dtype from tensor_meta.
     :type dtype: torch.dtype, optional
-
     :returns: The dequantized tensor.
     :rtype: torch.Tensor
     """
     device = tensor_meta.device
-    dtype = getattr(torch, tensor_meta.dtype) if dtype is None else dtype
+    output_dtype = (
+        getattr(torch, tensor_meta.dtype) if output_dtype is None else output_dtype
+    )
 
-    if device == "cpu":
-        tensor = minifloat_fake.compose_minifloat_component(
-            elements,
-            tensor_meta.meta,
-            dtype
+    if device.startswith("cuda"):
+        tensor = fp_kernels.compose_minifloat_component(
+            element, tensor_meta.meta, output_dtype=output_dtype
         )
     else:
-        tensor = minifloat_fake.compose_minifloat_component(
-            elements,
-            tensor_meta.meta,
-            dtype
+        tensor = fp_fake.compose_minifloat_component(
+            element, tensor_meta.meta, output_dtype=output_dtype
         )
-
-    tensor = tensor.to(dtype=dtype)
     return tensor
 
 
 def minifloat_quantizer_sim(
     tensor: Tensor,
-    block_dim: int,
     minifloat_meta: MinifloatMeta,
-    dtype: torch.dtype | None = None,
+    output_dtype: torch.dtype | None = None,
 ) -> Tensor:
     """
-    Quantizes and dequantizes a tensor using the MXFP format.
+    Quantize and dequantize a tensor using minifloat format.
 
-    :param tensor: The input tensor to be quantized.
+    :param tensor: The input tensor to quantize and dequantize.
     :type tensor: torch.Tensor
-    :param block_dim: The dimension to group the tensor elements into blocks.
-    :type block_dim: int
-    :param minifloat_meta: The metadata for the Minifloat format.
+    :param minifloat_meta: The metadata for the minifloat format.
     :type minifloat_meta: MinifloatMeta
-    :param dtype: The desired data type of the output tensor, by default None, which uses the dtype from minifloat_meta.
-
+    :param output_dtype: The desired data type of the output tensor, by default None, which uses the dtype from tensor_meta.
+    :type dtype: torch.dtype, optional
     :returns: The dequantized tensor.
     :rtype: torch.Tensor
     """
-    tensor, tensor_meta = extract_minifloat_components(tensor, minifloat_meta)
-    tensor_dq = compose_minifloat_tensor(tensor, tensor_meta, dtype=dtype)
-    return tensor_dq
-
+    element, tensor_meta = extract_minifloat_component(tensor, minifloat_meta)
+    return compose_minifloat_component(element, tensor_meta, output_dtype=output_dtype)
